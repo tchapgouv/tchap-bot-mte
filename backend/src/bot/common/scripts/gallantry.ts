ca091e656135c2ee3fb769e532935f1da8c2d6a7
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-05-14 17:30:01 +0200

    maj gallantry.ts

--- backend/src/bot/common/scripts/gallantry.ts
+++ backend/src/bot/common/scripts/gallantry.ts
@@ -8,7 +8,7 @@ import {addEmoji} from "../helper.js";
  */
 export function sayGoodbyeIfNecessary(client: MatrixClient, event: MatrixEvent, body: string) {
 
-    const regex: RegExp = /.*(bonne soirée|[aà] demain|bon we|bonsoir).*/i
+    const regex: RegExp = /.*(bonne soirée|[aà] demain|bon we|bonsoir|tcho|tchao|ciao).*/i
 
     if (regex.test(body)) {
         logger.debug("Saying goodbye.")
