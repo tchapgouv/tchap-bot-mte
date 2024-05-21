e88669e71d61d1e62aa25e2466093e45435cbf70
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-05-21 12:04:32 +0200

    check botname in lower case

--- backend/src/bot/common/Bot.ts
+++ backend/src/bot/common/Bot.ts
@@ -112,7 +112,7 @@ export class Bot {
                 const userIds = event.getContent()["m.mentions"]?.user_ids;
 
                 let isSelfMentioned = userIds && userIds.indexOf(botId) > -1;
-                if (isSelfMentioned === undefined && botName) isSelfMentioned = event.event.content?.body.toLowerCase().includes(botName)
+                if (isSelfMentioned === undefined && botName) isSelfMentioned = event.event.content?.body.toLowerCase().includes(botName.toLowerCase())
 
                 logger.debug("Is self mentioned ? ", isSelfMentioned)
                 logger.debug("sender = ", event.getSender())
