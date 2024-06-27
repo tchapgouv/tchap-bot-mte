d84573a07ed45d82d9f708b8cabe4fed30ed8356
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-06-27 10:30:47 +0200

    rejectUnauthorized

--- backend/src/utils/fetchWithError.ts
+++ backend/src/utils/fetchWithError.ts
@@ -1,15 +1,21 @@
 import fetch, {RequestInit} from 'node-fetch';
 import {HttpsProxyAgent} from 'https-proxy-agent';
+import https from "https";
 
-export default function (url: string, opts: { requestInit?: RequestInit, timeout?: number, proxify?: boolean , proxy?: string } = {}): Promise<any> {
+export default function (url: string, opts: { requestInit?: RequestInit, timeout?: number, proxify?: boolean, proxy?: string, disableSslCheck?: boolean } = {}): Promise<any> {
 
     if (!opts.requestInit) opts.requestInit = {method: "GET"}
     if (!opts.timeout) opts.timeout = 7000
     if (!opts.proxify) opts.proxify = false
+    if (!opts.disableSslCheck) opts.disableSslCheck = false
 
     if (opts.proxify) {
         const proxy = opts.proxy ? opts.proxy : process.env.PROXY_URL
-        opts.requestInit.agent = proxy ? new HttpsProxyAgent(proxy) : undefined
+        opts.requestInit.agent = proxy ? new HttpsProxyAgent(proxy, {rejectUnauthorized: opts.disableSslCheck}) : undefined
+    } else if (opts.disableSslCheck) {
+        opts.requestInit.agent = new https.Agent({
+            rejectUnauthorized: false,
+        })
     }
     opts.requestInit.method = opts.requestInit.method ? opts.requestInit.method : "GET"
 
