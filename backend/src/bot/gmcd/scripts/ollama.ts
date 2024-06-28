923ec8e2bb6363d90e8ed10b5d4bc3e939d7f268
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-06-28 10:08:21 +0200

    Maj context

--- backend/src/bot/gmcd/scripts/ollama.ts
+++ backend/src/bot/gmcd/scripts/ollama.ts
@@ -5,7 +5,7 @@ import logger from "../../../utils/logger.js";
 
 /**
  * Initialisation du contexte de réponse. Récupérer body.context :
- * curl http://ollama:11434/api/generate -d '{ "model": "llama3", "prompt": "Bonjour. Répond toujours en français. Ton nom est Bot-GMCD. Lis la documentation suivante en suivant les liens de manière récursive tant qu''ils commencent par la même racine : https://fabrique-numerique.gitbook.io/bnum puis répond à mes questions en se basant sur cette documentation. Ne précise pas que tu lis la documentation dans tes réponses. Si tu fais référence à la documentation dans ta réponse, donne un lien vers la page utile à la fin de ta réponse.", "stream": false }'
+ * curl http://ollama:11434/api/generate -d '{ "model": "llama3", "prompt": "Bonjour. Répond toujours en français. Ton nom est Bot-GMCD. Lis la documentation suivante en suivant les liens de manière récursive tant qu''ils commencent par la même racine : https://fabrique-numerique.gitbook.io/bnum puis répond à mes questions en se basant sur cette documentation. Ne précise pas que tu lis la documentation dans tes réponses. Si tu fais référence à la documentation dans ta réponse, donne un lien vers la page que tu as utilisé à la fin de ta réponse.", "stream": false }'
  */
 
 
@@ -30,7 +30,7 @@ export function ollama(client: MatrixClient, roomId: string, sender: any, body: 
                     body: JSON.stringify({
                         "model": "llama3",
                         "prompt": body.replace(/ollama /ig, ""),
-                        "context": [128006, 882, 128007, 271, 82681, 13, 51223, 3595, 44093, 665, 55467, 13, 31816, 9859, 1826, 11164, 12279, 44, 6620, 13, 44172, 1208, 9904, 46932, 5048, 665, 46932, 519, 3625, 908, 729, 409, 85722, 9517, 66, 16921, 37622, 934, 8839, 1081, 27462, 1370, 1208, 27584, 9148, 483, 551, 3788, 1129, 37955, 34395, 32294, 261, 2428, 33899, 2239, 4340, 3554, 2470, 44829, 75871, 3869, 11083, 4860, 665, 513, 3122, 519, 1765, 20662, 9904, 13, 4275, 51625, 1082, 6502, 1744, 9964, 41380, 1208, 9904, 7010, 51309, 9517, 23475, 13, 71083, 653, 55520, 5553, 1208, 2199, 409, 1208, 9904, 3869, 1208, 1913, 409, 9637, 90509, 13, 128009, 128006, 78191, 128007, 271, 82681, 758, 14465, 36731, 11164, 12279, 44, 6620, 11, 4864, 91507, 75871, 265, 3869, 26317, 4860, 665, 46932, 519, 3625, 11470, 409, 1208, 9904, 426, 17946, 382, 47, 283, 19610, 45325, 757, 1153, 261, 15265, 55133, 3488, 949, 128009],
+                        "context": [128006, 882, 128007, 271, 82681, 13, 51223, 3595, 44093, 665, 55467, 13, 31816, 9859, 1826, 23869, 12279, 44, 6620, 13, 44172, 1208, 9904, 46932, 5048, 665, 46932, 519, 3625, 908, 729, 409, 85722, 9517, 66, 16921, 37622, 934, 8839, 1081, 27462, 1370, 1208, 27584, 9148, 483, 551, 3788, 1129, 37955, 34395, 32294, 261, 2428, 33899, 2239, 4340, 3554, 2470, 44829, 75871, 3869, 11083, 4860, 665, 513, 3122, 519, 1765, 20662, 9904, 13, 4275, 51625, 1082, 6502, 1744, 9964, 41380, 1208, 9904, 7010, 51309, 9517, 23475, 13, 12095, 9964, 66517, 91558, 16271, 3869, 1208, 9904, 7010, 9637, 90509, 11, 21559, 653, 55520, 5553, 1208, 2199, 1744, 9964, 439, 42587, 978, 3869, 1208, 1913, 409, 9637, 90509, 13, 128009, 128006, 78191, 128007, 271, 82681, 758, 14465, 36731, 23869, 12279, 44, 6620, 13, 14465, 91507, 75289, 466, 1208, 9904, 1880, 9189, 75871, 265, 665, 55467, 13, 3489, 6853, 1826, 15265, 55133, 3488, 949, 128009],
                         "stream": false
                     })
                 }
