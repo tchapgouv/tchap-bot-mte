e91d38c2a51c1f09af8f5f0bc2f598c6eb57fc09
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-07-11 12:37:08 +0200

    Get users with ldap request

--- backend/src/services/ldap.service.ts
+++ backend/src/services/ldap.service.ts
@@ -49,6 +49,65 @@ export default {
         return mailPR
     },
 
+    // await ldapService.getUsersWithBaseDn(ldap.createClient({url: process.env.LDAP_URI || ''}), "ou=GMCD,ou=DETN,ou=UNI,ou=DNUM,ou=SG,ou=AC,ou=melanie,ou=organisation,dc=equipement,dc=gouv,dc=fr", true).then(value => console.log("value"))
+    async getUsersWithLdapRequest(client: ldap.Client, baseDn: string, recursively = false, filter: "(&(objectClass=mineqPerson))") {
+
+        logger.debug("getUsersWithBaseDn", baseDn)
+
+        const opts: SearchOptions = {
+            attributes: ['uid', 'cn', 'mailPR', 'displayName'],
+            filter,
+            scope: recursively ? 'sub' : 'one'
+        };
+
+        logger.notice('LDAP : Search users in ' + baseDn)
+
+        let userCount = 0;
+
+        let users: any[] = []
+
+        return new Promise((resolve, reject) => {
+
+            (async () => {
+                client.search(baseDn, opts, ((err, res) => {
+
+                    res.on('searchRequest', (searchRequest) => {
+                        logger.debug('LDAP : searchRequest: ', searchRequest.messageId);
+                    });
+                    res.on('searchReference', (referral) => {
+                        logger.debug('LDAP : referral: ' + referral.uris.join());
+                    });
+                    res.on('error', (err) => {
+                        logger.error('LDAP : error: ' + err.message);
+                    });
+                    res.on('end', (result) => {
+                        logger.debug('LDAP : status: ' + result?.status);
+                        if (userCount === 0) {
+                            logger.warning('LDAP : no user found.')
+                            reject('LDAP : no user found.')
+                        }
+                        resolve(users)
+                    });
+
+                    res.on('searchEntry', (entry) => {  // There was a match.
+
+                        userCount++
+
+                        let mappedUser: any = {}
+
+                        mappedUser.dn = entry.pojo.objectName
+
+                        for (const attribute in entry.pojo.attributes) {
+                            mappedUser[entry.pojo.attributes[attribute].type] = entry.pojo.attributes[attribute].values
+                        }
+                        logger.debug('LDAP : User found : ', mappedUser)
+                        users.push(mappedUser)
+                    })
+                }))
+            })()
+        })
+    },
+
     async getUserForUID(client: ldap.Client, username: string) {
 
         logger.debug("getUserForUID", username)
@@ -102,5 +161,6 @@ export default {
             })()
         })
     }
-
 }
+
+
