0400d19de8255d5922a6871011586aad71c1155f
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-07-11 17:10:00 +0200

    stats

---
+++ backend/src/bot/psin/scripts/stats.ts
@@ -0,0 +1,43 @@
+import {MatrixClient, MatrixEvent} from "matrix-js-sdk";
+import fetchWithError from "../../../utils/fetchWithError.js";
+import {sendHtmlMessage} from "../../common/helper.js";
+import logger from "../../../utils/logger.js";
+
+
+/**
+ * @help
+ * command : stats
+ * return : je retourne la liste statistiques en cours
+ * isAnswer : true
+ */
+export function listIncidentsIfAsked(client: MatrixClient, event: MatrixEvent, body: string) {
+
+    const regex: RegExp = /.*(stats).*/i
+
+
+    if (regex.test(body)) {
+
+        if (event?.sender?.name &&
+            event?.sender?.userId &&
+            event?.event?.room_id) {
+
+            const roomId = event.event.room_id
+
+            const psinApiKey = process.env.PSIN_API_KEY || ""
+
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+            fetchWithError("https://psin.supervision.e2.rie.gouv.fr/centreon/ApiPsin.php?cle=" + psinApiKey + "&indic=stats", {proxify: true})
+                .then((value: Response) => {
+                    value.text().then(decodedGzip => sendHtmlMessage(client, roomId, decodedGzip, decodedGzip))
+                }).catch(reason => {
+                logger.error("Error listing stats :", reason)
+            }).finally(() => {
+                process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1'
+            })
+
+            return true
+        }
+    }
+
+    return false
+}
