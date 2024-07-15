483d61486b373b2868265d6596197a7b8cfde613
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2024-07-15 15:35:28 +0200

    Wake ollama up every day

--- backend/src/Crontab.ts
+++ backend/src/Crontab.ts
@@ -3,6 +3,8 @@ import logger from "./utils/logger.js";
 import ldapGroupService from "./services/ldapListGroup.service.js";
 import botService from "./services/bot.service.js";
 import mailGroupService from "./services/mailListGroup.service.js";
+import fetchWithError from "./utils/fetchWithError.js";
+import {CONTEXT} from "./bot/gmcd/scripts/ollama.js";
 
 const MIDNIGHT = '0 0 * * *'
 
@@ -32,15 +34,47 @@ function updateMailListGroups() {
     })
 }
 
+function wakeOllamaUp() {
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+    fetchWithError('https://Ollama.gmcd-runner01.eco4.cloud.e2.rie.gouv.fr/api/generate',
+        {
+            proxify: true,
+            timeout: 60 * 1000,
+            requestInit: {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "model": "llama3",
+                    "prompt": "Bonjour Bot-GMCD, comment vas tu aujourd'hui ?",
+                    "context": CONTEXT,
+                    "stream": false,
+                    "keep_alive": "24h"
+                })
+            }
+        }
+    )
+        .then(response => response.json())
+        .then(data => {
+            logger.notice("Ollama wake up response : ", data.response)
+        })
+        .catch(reason => {
+            logger.warning("Ollama wake up error : ", reason)
+        })
+}
+
 export default class Crontab {
 
     init() {
 
         crontab.schedule(MIDNIGHT, () => {
 
-            logger.notice('Running midnight tasks');
-            updateLdapListGroups();
-            updateMailListGroups();
+            logger.notice('Running midnight tasks')
+            updateLdapListGroups()
+            updateMailListGroups()
+            wakeOllamaUp()
         });
 
     }
