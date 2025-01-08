8448ffd65d70b564b36c40023146e07938f556c8
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2025-01-08 15:31:07 +0100

    :lipstick: table title

--- frontend/src/views/Webhooks.vue
+++ frontend/src/views/Webhooks.vue
@@ -43,7 +43,7 @@
               'Room Id', 
               'Bot Id', 
               'Action']"
-             title="''"
+             title=""
              :rows="filteredWebhooks"
              v-if="filteredWebhooks.length > 0"/>
 
