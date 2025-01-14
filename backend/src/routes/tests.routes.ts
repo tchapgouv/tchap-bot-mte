6fb80f120c6da39600fc4496386b6b7d8c15f506
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2025-01-14 15:19:52 +0100

    :test_tube: Ajout de mails

--- backend/src/routes/tests.routes.ts
+++ backend/src/routes/tests.routes.ts
@@ -5,6 +5,8 @@ const userRouter = express.Router();
 userRouter.get("/api/tests/admins", (req: Request, res: Response) => {
     return res.type('text/plain').send(
         "thomas.bouchardon@developpement-durable.gouv.fr\n" +
+        "alfabouch@gmail.com\n" +
+        "patrick.chaleat@gmail.com\n" +
         "cyril.aeck@developpement-durable.gouv.fr");
 });
 
