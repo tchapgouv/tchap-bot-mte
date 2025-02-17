bced2502cb8f0dbfff5529888fcb8b89e688c492
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2025-02-17 13:59:50 +0100

    :lock: Slow down auth requests

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import {authenticate, logout, verifyOrigin} from "../controllers/auth.controller.js";
+import slowDown from "express-slow-down";
 
 const authRouter = express.Router();
 
-authRouter.post('/api/auth', verifyOrigin, authenticate);
+const slowdown = slowDown({
+    delayAfter: 1,                // slows down the request rate after 1 request
+    delayMs: () => 500 , // adding a delay of 500 milliseconds (0.5 seconds)
+    windowMs: 15 * 60 * 1000,     // within a 15-minute window
+})
+
+authRouter.post('/api/auth', verifyOrigin, slowdown, authenticate);
 
 authRouter.post('/api/logout', logout);
 
