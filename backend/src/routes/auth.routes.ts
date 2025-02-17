905c1930cd54a1ab3850ab1f20a52bdd2892b1a8
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2025-02-17 09:35:23 +0100

    :lock: Auth only from intranet

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import {authenticate, logout} from "../controllers/auth.controller.js";
+import {authenticate, logout, verifyOrigin} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/api/auth', authenticate);
+authRouter.post('/api/auth', verifyOrigin, authenticate);
 
 authRouter.post('/api/logout', logout);
 
