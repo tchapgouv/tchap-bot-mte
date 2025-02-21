0482fe5a4ead9c2cdb7622c7a2c579efce484bb1
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2025-02-21 11:44:19 +0100

    :bug: Catch invite

--- backend/src/bot/common/scripts/invite.ts
+++ backend/src/bot/common/scripts/invite.ts
@@ -1,6 +1,7 @@
 import {MatrixClient} from "matrix-js-sdk";
-import {isSupport, sendMessage} from "../helper.js";
-import botGmcd from "../../gmcd/bot.js";
+import {getPowerLevel, isSupport, sendMessage} from "../helper.js";
+import botService from "../../../services/bot.service.js";
+import logger from "../../../utils/logger.js";
 
 export function inviteInRoomIfAsked(client: MatrixClient, roomId: string, userId: string, body: string) {
 
@@ -8,17 +9,43 @@ export function inviteInRoomIfAsked(client: MatrixClient, roomId: string, userId
 
     if (roomId && body && inviteInRoomOptions.some(option => body.includes(option))) {
 
+        // Bot-Gmcd [Developpement-Durable] invite @quentin.rabier-developpement-durable.gouv.fr:agent.dev-durable.tchap.gouv.fr !GCDRinCBfdhIEpoecb:agent.dev-durable.tchap.gouv.fr
         // @quentin.rabier-developpement-durable.gouv.fr:agent.dev-durable.tchap.gouv.fr
-        // !PHwHATDnuimhMowBKN:agent.dev-durable.tchap.gouv.fr
+        // !GCDRinCBfdhIEpoecb:agent.dev-durable.tchap.gouv.fr
 
         let roomToInvite = body.replace(/.*invite +@.*? +(!.*?)(?: |$).*/, "$1");
         let userToInvite = body.replace(/.*invite +(@.*?) +!.*?(?: |$).*/, "$1");
 
         if (isSupport(userId)) {
 
-            botGmcd.client.invite(roomToInvite, userToInvite).then(() => {
-                sendMessage(botGmcd.client, roomId, `L'utilisateur ${userToInvite} a bien été invité dans la salle ${roomToInvite} ! 🎉`)
+            botService.getRoomName(roomToInvite).catch(() => {
+                sendMessage(client, roomId, `Désolé, je ne connais pas le salon ${roomToInvite} ! 🤷`)
+                return true
             })
+
+            botService.isMemberOfRoom(roomToInvite, client.getUserId() + '').then(isMember => {
+
+                if (!isMember) {
+                    sendMessage(client, roomId, `Désolé, je ne suis pas membre de ${roomToInvite} ! 🤷`)
+                    return true
+                }
+
+                getPowerLevel(client, roomId).then(powerLevel => {
+
+                    if (powerLevel < 100) {
+                        sendMessage(client, roomId, `Désolé, je ne suis pas administrateur de ${roomToInvite} ! 🤷`)
+                        return true
+                    }
+
+                    client.invite(roomToInvite, userToInvite).then(() => {
+                        sendMessage(client, roomId, `L'utilisateur ${userToInvite} a bien été invité dans la salle ${roomToInvite} ! 🎉`)
+                    }).catch(reason => {
+                        sendMessage(client, roomId, "Une erreur est survenue ! 🤷")
+                        logger.error(`Inviting ${userToInvite} in ${roomToInvite} `, reason)
+                    })
+                })
+            })
+
 
         } else {
             sendMessage(client, roomId, "Désolé, seul un membre du support pour réaliser cette action ! 🤷")
